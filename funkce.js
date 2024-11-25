92abddc471f4a7fcfdb337058ced7cedc0776a9b
Vena07 <vasiknosek@email.cz>
2024-11-25 21:30:12 +0100

    absence add

--- funkce.js
+++ funkce.js
@@ -4,16 +4,25 @@ function zavrit(cislo){
             document.getElementById("rozvrh").disabled = true;
             document.getElementById("znamky").disabled = false;
             document.getElementById("absence").disabled = false;
+            document.getElementById("rozvrhstr").style.display = "flex";
+            document.getElementById("znamkystr").style.display = "none";
+            document.getElementById("absencestr").style.display = "none";
             break
         case 2:
             document.getElementById("rozvrh").disabled = false;
             document.getElementById("znamky").disabled = true;
             document.getElementById("absence").disabled = false;
+            document.getElementById("rozvrhstr").style.display = "none";
+            document.getElementById("znamkystr").style.display = "flex";
+            document.getElementById("absencestr").style.display = "none";
             break
         case 3:
             document.getElementById("rozvrh").disabled = false;
             document.getElementById("znamky").disabled = false;
             document.getElementById("absence").disabled = true;
+            document.getElementById("rozvrhstr").style.display = "none";
+            document.getElementById("znamkystr").style.display = "none";
+            document.getElementById("absencestr").style.display = "flex";
             break
     }
 }
@@ -82,6 +91,31 @@ function save() {
 
 function closeForm(){
     document.getElementById('form').style.display = 'none';
+}
+        
+function otevritAbsenciForm() {
+    document.getElementById('absenceForm').style.display = 'flex';
+}
+
+function zavritAbsenci() {
+    document.getElementById('absenceForm').style.display = 'none';
+}
+
+function pridatAbsenci() {
+    const date = document.getElementById('absenceDate').value;
+    const hour = document.getElementById('absenceHour').value;
+    const isExcused = document.getElementById('isExcused').checked;
+    const absenceTable = document.getElementById('absenceTable');
+
+    const newRow = absenceTable.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = `${date}  |  ${hour}`;
+    cell2.innerHTML = isExcused ? 'Omluvená' : 'Neomluvená';
+    cell2.className = isExcused ? 'absence-green' : 'absence-red';
+
+    document.getElementById('absenceForm').style.display = 'none';
 }
 
 zmena(1);
